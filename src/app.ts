db8b7f15b6a580fc9c141434dabe59070b015461
gnarargs <hunter@initialcapacity.io>
2024-02-18 12:17:53 -0800

    Add first leg of oauth flow

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,18 @@ import 'dotenv/config'
 const app = express()
 
 app.get('/', (req, res) => {
-  const foo = process.env.FOO
-  const token = req.query.token
-  res.send(`Token is ${token}, foo is: ${foo}`)
+  const authBaseUrl = process.env.AUTH0_DOMAIN
+  const authClientID = process.env.AUTH0_CLIENT_ID
+  const authRedirectUri = process.env.AUTH0_REDIRECT_URI
+
+  const authRedirect = `https://${authBaseUrl}/authorize?response_type=code&client_id=${authClientID}&redirect_uri=${authRedirectUri}`
+
+  res.redirect(authRedirect)
+})
+
+app.get('/callback', (req, res) => {
+  console.log(req.params)
+  res.send('OK')
 })
 
 app.listen(3000, () => {
